9e66266c8acd879357a5acb96182d55595657553
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-07-07 12:29:40 +0600

    py-10 md:py-18

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -2,7 +2,7 @@
 
 const PageLayout = ({children}) => {
     return (
-        <div className="max-w-7xl mx-auto py-6 md:py-10 px-4 xl:px-0">
+        <div className="max-w-7xl mx-auto py-10 md:py-18 px-4 xl:px-0">
             {children}
         </div>
     );
